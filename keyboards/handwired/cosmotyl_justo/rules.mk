0da4cffa6c6989d4eb59d56d0099869add6a6454
Arunas Smaliukas <arunas@smala.lt>
2025-12-25 11:08:40 +0200

    rules

---
+++ keyboards/handwired/cosmotyl_justo/rules.mk
@@ -0,0 +1,20 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+RGBLIGHT_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+
+# Trackball
+# POINTING_DEVICE_ENABLE = yes
+# POINTING_DEVICE_DRIVER = pmw3389 # Change to pmw3360 if you use that
+
+# Encoder
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+CONSOLE_ENABLE=yes

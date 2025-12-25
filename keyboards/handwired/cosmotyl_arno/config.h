69425d790898395a4d936d2363a2e104a2644137
Arunas Smaliukas <arunas@smala.lt>
2025-12-25 11:02:13 +0200

    config pins

--- keyboards/handwired/cosmotyl_arno/config.h
+++ keyboards/handwired/cosmotyl_arno/config.h
@@ -3,13 +3,13 @@
 #define EE_HANDS // Store which side I am in EEPROM
 
 /* Keyboard matrix assignments */
-#define MATRIX_ROW_PINS { GP22, GP14, GP15, GP13, GP12 }
-#define MATRIX_COL_PINS { GP16, GP17, GP18, GP19, GP20, GP21 }
+#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7 }
 
-#define MATRIX_ROW_PINS_RIGHT { GP12, GP21, GP15, GP14, GP13 }
-#define MATRIX_COL_PINS_RIGHT { GP22, GP20, GP19, GP18, GP17, GP16 }
+/* Encoders */
+#define ENCODERS_PAD_A { GP21 }
+#define ENCODERS_PAD_B { GP20 }
 
 /* Reset */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-// This LED blinks when entering bootloader
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17// This LED blinks when entering bootloader

8c555ff5fd151e00f111fed2a38cb8e2c9a11155
Arunas Smaliukas <arunas@smala.lt>
2025-12-25 11:07:18 +0200

    wrong folder

---
+++ keyboards/handwired/cosmotyl_justo/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define EE_HANDS // Store which side I am in EEPROM
+
+/* Keyboard matrix assignments */
+#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP22 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7 }
+
+/* Encoders */
+#define ENCODERS_PAD_A { GP21 }
+#define ENCODERS_PAD_B { GP20 }
+
+/* Reset */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // This LED blinks when entering bootloader
+
+// TODO: OLED, SCL=GP19 SDA=GP18
